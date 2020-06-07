feb034536ec234c5ed1e7401815359d93a5139a4
devig <golovnevivan@mail.ru>
2020-06-08 01:20:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const http = require("http");
+const http = require("https");
 const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
